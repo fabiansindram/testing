71b4e83e7ab63b008094729017c736d339ec6e45
f.sindram <f.sindram@student.vu.nl>
2021-10-08 01:22:45 +0200

    lolo

--- werkis.cpp
+++ werkis.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
 
    int numSpelers;
+   string spc = ", ";
 
    cout << "hoeveel spelers?";
    cin >> numSpelers;
@@ -14,61 +16,227 @@ int main(){
       string d4;
       string e5;
       string f6;
+      string g7;
+      string h8;
+      string i9;
+      string j10;
+      string k11;
 
   switch(numSpelers){
 
- case 5:  
+ case 7:
       
-
-      cout << "wie is speler 1" << endl;
+      cout << "wie is speler 1? " << endl;
       cin >> a1;
       
-      cout << "wie is speler 2?" << endl;
+      cout << "wie is speler 2? " << endl;
+      cin >> b2;
+      
+      cout << "wie is speler 3? " << endl;
+      cin >> c3;
+      
+      cout << "wie is speler 4? " << endl;
+      cin >> d4;
+      
+      cout << "wie is speler 5? " << endl;
+      cin >> e5;
+      
+      cout << "wie is speler 6? " << endl;
+      cin >> f6;
+      
+      cout << "wie is speler 7? " << endl;
+      cin >> g7;
+
+    cout << "1e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << e5 << endl;
+    cout << "2e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << f6 << " en " << g7 << endl;
+    cout << "3e speelronde spelen: " << a1 << spc << d4 << spc << e5 << spc << f6 << " en " << g7 << endl;
+    cout << "6e speelronde spelen: " << b2 << spc << c3 << spc << d4 << spc << e5 << " en " << f6 << endl;
+    cout << endl;
+    cout << "5e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << g7 << endl;
+    cout << "2e speelronde spelen: " << a1 << spc << b2 << spc << e5 << spc << f6 << " en " << g7 << endl;
+    cout << "2e speelronde spelen: " << c3 << spc << d4 << spc << e5 << spc << f6 << " en " << g7 << endl;
+    cout << "8e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << e5 << endl;
+
+   break;
+
+ case 8:  
+      
+
+      cout << "wie is speler 1? " << endl;
+      cin >> a1;
+      
+      cout << "wie is speler 2? " << endl;
       cin >> b2;
 
-      cout << "wie is speler 3?" << endl;
+      cout << "wie is speler 3? " << endl;
       cin >> c3;
 
       cout << "wie is speler 4? " << endl;
       cin >> d4;
 
-      cout << "wie is speler 5?" << endl;
+      cout << "wie is speler 5? " << endl;
       cin >> e5;
-
-      cout << "speler" << a1 << b2 << c3 << endl;
-      break;
-   
-
-     case 6: 
       
-      
-      cout << "wie is speler 1" << endl;
-      cin >> a1;
-      
-      cout << "wie is speler 2?" << endl;
-      cin >> b2;
-
-      cout << "wie is speler 3?" << endl;
-      cin >> c3;
-
-      cout << "wie is speler 4? " << endl;
-      cin >> d4;
-
-      cout << "wie is speler 5?" << endl;
-      cin >> e5;
-
-      cout << "wie is speler 6?" << endl;
+      cout << "wie is speler 6? " << endl;
       cin >> f6;
 
-      cout << "speler" << a1 << b2 << c3 << endl;
-   
+      cout << "wie is speler 7? " << endl;
+      cin >> g7;
+
+      cout << "wie is speler 8? " << endl;
+      cin >> h8;
+      
+      cout << "1e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << e5 << endl;
+      cout << "2e speelronde spelen: " << a1 << spc << b2 << spc << f6 << spc << g7 << " en " << h8 << endl;
+      cout << "3e speelronde spelen: " << c3 << spc << d4 << spc << e5 << spc << f6 << " en " << g7 << endl;
+      cout << "4e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << h8 << endl;
+      cout << endl;
+      cout << "5e speelronde spelen: " << a1 << spc << e5 << spc << f6 << spc << g7 << " en " << h8 << endl;
+      cout << "6e speelronde spelen: " << b2 << spc << c3 << spc << d4 << spc << e5 << " en " << f6 << endl;
+      cout << "7e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << g7 << " en " << h8 << endl;
+      cout << "7e speelronde spelen: " << d4 << spc << e5 << spc << f6 << spc << g7 << " en " << h8 << endl;
       break;
+   
 
+     case 9: 
+      
+      
+      cout << "wie is speler 1? " << endl;
+      cin >> a1;
+      
+      cout << "wie is speler 2? " << endl;
+      cin >> b2;
 
+      cout << "wie is speler 3? " << endl;
+      cin >> c3;
+
+      cout << "wie is speler 4? " << endl;
+      cin >> d4;
+
+      cout << "wie is speler 5?" << endl;
+      cin >> e5;
+
+      cout << "wie is speler 6? " << endl;
+      cin >> f6;
+      
+      cout << "wie is speler 7? " << endl;
+      cin >> g7;
+      
+      cout << "wie is speler 8? " << endl;
+      cin >> h8;
+      
+      cout << "wie is speler 9? " << endl;
+      cin >> i9;
+      
+
+      cout << "1e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << e5 << endl;
+      cout << "2e speelronde spelen: " << f6 << spc << g7 << spc << a1 << spc << h8 << " en " << i9 << endl;
+      cout << "3e speelronde spelen: " << b2 << spc << c3 << spc << d4 << spc << e5 << " en " << f6 << endl;
+      cout << "4e speelronde spelen: " << a1 << spc << b2 << spc << g7 << spc << h8 << " en " << i9 << endl;
+      cout << endl;
+      cout << "5e speelronde spelen: " << c3 << spc << d4 << spc << e5 << spc << f6 << " en " << g7 << endl;
+      cout << "6e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << h8 << " en " << i9 << endl;
+      cout << "7e speelronde spelen: " << d4 << spc << e5 << spc << f6 << spc << g7 << " en " << h8 << endl;
+      cout << "8e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << i9 << endl;
+     break;
+   
+   
+   case 10:
+      
+      cout << "wie is speler 1? " << endl;
+      cin >> a1;
+      
+      cout << "wie is speler 2? " << endl;
+      cin >> b2;
+      
+      cout << "wie is speler 3? " << endl;
+      cin >> c3;
+      
+      cout << "wie is speler 4? " << endl;
+      cin >> d4;
+      
+      cout << "wie is speler 5? " << endl;
+      cin >> e5;
+      
+      cout << "wie is speler 6? " << endl;
+      cin >> f6;
+      
+      cout << "wie is speler 7? " << endl;
+      cin >> g7;
+      
+      cout << "wie is speler 8? " << endl;
+      cin >> h8;
+      
+      cout << "wie is speler 9? " << endl;
+      cin >> i9;
+
+      cout << "wie is speler 10? " << endl;
+      cin >> j10;
+      
+      cout << "1e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << e5 << endl;
+      cout << "2e speelronde spelen: " << f6 << spc << g7 << spc << h8 << spc << i9 << " en " << j10 << endl;
+      cout << "3e speelronde spelen: " << b2 << spc << c3 << spc << d4 << spc << e5 << " en " << f6 << endl;
+      cout << "4e speelronde spelen: " << a1 << spc << g7 << spc << h8 << spc << i9 << " en " << j10 << endl;
+      cout << endl;
+      cout << "5e speelronde spelen: " << c3 << spc << d4 << spc << e5 << spc << f6 << " en " << g7 << endl;
+      cout << "6e speelronde spelen: " << a1 << spc << b2 << spc << h8 << spc << i9 << " en " << j10 << endl;
+      cout << "7e speelronde spelen: " << d4 << spc << e5 << spc << f6 << spc << g7 << " en " << h8 << endl;
+      cout << "8e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << i9 << " en " << j10 << endl;
+    
+     break;
+  
+   case 11:
+      
+      cout << "wie is speler 1? " << endl;
+      cin >> a1;
+      
+      cout << "wie is speler 2? " << endl;
+      cin >> b2;
+      
+      cout << "wie is speler 3? " << endl;
+      cin >> c3;
+      
+      cout << "wie is speler 4? " << endl;
+      cin >> d4;
+      
+      cout << "wie is speler 5? " << endl;
+      cin >> e5;
+      
+      cout << "wie is speler 6? " << endl;
+      cin >> f6;
+      
+      cout << "wie is speler 7? " << endl;
+      cin >> g7;
+      
+      cout << "wie is speler 8? " << endl;
+      cin >> h8;
+      
+      cout << "wie is speler 9? " << endl;
+      cin >> i9;
+
+      cout << "wie is speler 10? " << endl;
+      cin >> j10;
+   
+      cout << "Wie is speler 11? " << endl;
+      cin >> k11;
+
+      cout << "1e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << e5 << endl;
+      cout << "2e speelronde spelen: " << f6 << spc << g7 << spc << h8 << spc << i9 << " en " << j10 << endl;
+      cout << "3e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << d4 << " en " << k11 << endl;
+      cout << "4e speelronde spelen: " << e5 << spc << f6 << spc << g7 << spc << h8 << " en " << i9 << endl;
+      cout << endl;
+      cout << "5e speelronde spelen: " << a1 << spc << b2 << spc << c3 << spc << j10 << " en " << k11 << endl;
+      cout << "6e speelronde spelen: " << d4 << spc << e5 << spc << f6 << spc << g7 << " en " << h8 << endl;
+      cout << "7e speelronde spelen: " << a1 << spc << b2 << spc << i9 << spc << j10 << " en " << k11 << endl;
+      cout << "8e speelronde spelen: " << c3 << spc << d4 << spc << e5 << spc << f6 << " en " << g7 << endl;
+   
+   break;
+   
  default:
 
-      cout << "no";
+      cout << "je hebt teveel of te weinig spelers voor een slangensysteem!1";
    }
+return 0;
 
 }
   
